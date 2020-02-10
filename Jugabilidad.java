7f5e52db6f029189baacfcf59de5c89f8d4e96c7
AnemonaShin <deshkn@hotmail.com>
2020-02-10 07:55:25 +0000

    someday somewhere somehow i think i will add some claaaaaaaaaaaassseeees

--- Jugabilidad.java
+++ Jugabilidad.java
@@ -13,11 +13,14 @@ public class Jugabilidad{
       }
     }
   }
+  public static void GAMEOVER(){
+    Jugabilidad.BorrarPantalla();
+    System.out.println(">--------- GAME OVER ---------<");
+  }
   public static ArrayList Enemigos(){
     Random R = new Random();
     int MR = 0;
     boolean Enemigos = true;
-    String Nombre = "";
     ArrayList Datos = new ArrayList();
     //------------------------------------------
     ArrayList E1 = new ArrayList(); // Enemigo 1
@@ -61,7 +64,8 @@ public class Jugabilidad{
   public static void Batalla(){
     Scanner Read = new Scanner(System.in); 
     ArrayList Datos = Jugabilidad.Enemigos(); // Resibe datos de la Enemigos y los aloja en un ArrayList.
-    int Election = 0;
+    int ElectionN = 0;
+    String ElectionT = "";
     boolean Battle = true;
     boolean Verify = false;
     int Life = 0;
@@ -81,51 +85,75 @@ public class Jugabilidad{
     
       try {
         System.out.print("-------------------------- > [");
-        System.out.println("--------------["+(Election =   Read.nextInt())+"]--------------");
-        switch(Election){
-          case 1:
-          System.out.println("----- ¡Atacas al enemigo! -----");
-          System.out.println("---- ¡Le quitas 1 de vida! ----");
-          try {
-            Thread.sleep(2000);
-          } catch(Exception e){
+        System.out.println("--------------["+(ElectionT =   Read.nextLine())+"]--------------");
+        if (ElectionT.length()==1){
+          ElectionN = Integer.parseInt(ElectionT);
+          switch(ElectionN){
+            case 1:
+              System.out.println("----- ¡Atacas al enemigo! -----");
+              System.out.println("---- ¡Le quitas 1 de vida! ----");
+              try {
+                Thread.sleep(2000);
+              }  catch(Exception e){
 
-          }
-          Life = Life - 1;
-          Datos.set(1, Life);
-          if (Life == 0){
-            System.out.println("----- ¡Enemigo Derrotado! -----");
+              }
+              Life = Life - 1;
+              Datos.set(1, Life);
+              if (Life == 0){
+                System.out.println("----- ¡Enemigo Derrotado! -----");
+                Battle = false;
+              }
+            break;
+            case 2: // Para futuras practicas.
+              System.out.println("¡Aun no implementado!");
+              try {
+                Thread.sleep(3000);
+              } catch(Exception e){
+              
+              }
+            break;
+            case 3: // Necesario aprender como crear inventario. (Plan hacer otra funcion)
+              System.out.println("¡Aun no implementado!");
+              try {
+                Thread.sleep(3000);
+              } catch(Exception e){
+
+              } 
+            break;
+            case 4:
+              System.out.println("-------------------------------");
+              System.out.println("----- ¡Corres por tu vida! ----");
+              System.out.println("-------------------------------");
+            try {
+              Thread.sleep(3000);
+            } catch(Exception e){
+
+            }
             Battle = false;
-          }
-          break;
-          case 2: // Para futuras practicas.
-          System.out.println("¡Aun no implementado!");
-          try {
-            Thread.sleep(3000);
-          } catch(Exception e){
+            break;
+            default:
+              System.out.println("-------------------------------");
+              System.out.println("----- Opcion Inexistente ------");
+              System.out.println("-------------------------------");
+              try {
+                Thread.sleep(3000);
+              } catch(Exception e){
 
-          }
-          break;
-          case 3: // Necesario aprender como crear inventario. (Plan hacer otra funcion)
-          System.out.println("¡Aun no implementado!");
-          try {
-            Thread.sleep(3000);
-          } catch(Exception e){
-
+              } 
+            break;
+            }
           } 
-          break;
-          case 4:
-          System.out.println("-------------------------------");
-          System.out.println("----- ¡Corres por tu vida! ----");
-          System.out.println("-------------------------------");
-          try {
-            Thread.sleep(3000);
-          } catch(Exception e){
+          else {
+            System.out.println("-------------------------------");
+            System.out.println("------ Un numero a la vez -----");
+            System.out.println("-------------------------------");
+            try {
+                Thread.sleep(3000);
+              } catch(Exception e){
 
+              } 
           }
-          Battle = false;
-          break;
-        }
+        
       } 
       catch(Exception e){
         try {
@@ -137,8 +165,160 @@ public class Jugabilidad{
         System.out.println("------ ¡No ingrese texto! -----");
         System.out.println("-------------------------------");
         Jugabilidad.BorrarPantalla();
-        Read.nextLine();
       }
     }
+  }
+
+  public static boolean RatioDeEncuentroBatalla(){
+    boolean Acierto = true;
+    boolean Verify = true;
+    Random R = new Random();
+    int RR = 0;
+    while (Verify == true){
+      RR = R.nextInt(15);
+      if (RR < 5){ // Acierta Batalla
+        Acierto = true;
+        Verify = false;
+      }
+      else if(RR > 5){ // Evade Batalla
+        Acierto = false;
+        Verify = false;
+      }
+    }
+    return Acierto;
+  }
+
+  public static void AciertoBatalla(){
+    boolean Acierto = Jugabilidad.RatioDeEncuentroBatalla();
+    if (Acierto == true){
+      System.out.println("----- ¡Batalla Inminente! -----");
+      Jugabilidad.Batalla();
+    } else if(Acierto == false){
+
+    }
+  }
+  
+  public static ArrayList Jugador(String Nombre){
+    Scanner Read = new Scanner(System.in);
+    ArrayList DatosJ = new ArrayList();
+    ArrayList Stats = new ArrayList();
+    DatosJ.add(Nombre);
+    String Job = "";
+    int STR = 0;
+    int VIT = 0;
+    int LUCK = 0;
+    System.out.println("Bienvenido "+Nombre+"...");
+    System.out.println("Te encuentras encerrado en una habitacion sin salida.");
+    System.out.println("Solo tienes un par de cosas a tu alrededor...");
+    try {
+      Thread.sleep(2000);
+      System.out.println("");
+      System.out.print("[(1)]Un Zapato,");
+      System.out.print(" [(2)]Un Guante,");
+      System.out.print(" [(3)]Un Libro");
+      System.out.print(" y [(4)]Una Piedra");
+    } catch(Exception e){
+
+    }
+    System.out.println("");
+    System.out.println("Debes elegir uno...");
+    System.out.println("");
+    System.out.print("¿Cual eliges? > ");
+    try {
+      int Op = Read.nextInt();
+      switch (Op){
+      case 1: //Runner
+        Job = "Runner";
+        System.out.println("Recoges el Zapato");
+        DatosJ.add(Job);
+      break;
+      case 2: //Fighter
+        Job = "Fighter";
+        System.out.println("Recoges el Guante");
+        DatosJ.add(Job);
+      break;
+      case 3: //Mage
+        Job = "Mage";
+        System.out.println("Recoges el Libro");
+        DatosJ.add(Job);
+      break;
+      case 4: //Druid
+        Job = "Druid";
+        System.out.println("Recoges la Piedra");
+        DatosJ.add(Job);
+      break;
+      default: //Nonsense
+        Job = "Nonsense";
+        System.out.println("Preparate para la nada...");
+        DatosJ.add(Job);
+      break;
+      }
+      
+      switch(Job){
+        case "Runner":
+        System.out.println("Has elegido un camino lleno de escapes...");
+        STR = 1;
+        VIT = 3;
+        LUCK = 5;
+        // Inicio calculo de estadisticas principales.
+        Stats = Jugabilidad.Estadisticas(STR, VIT, LUCK);
+        System.out.println("Tus estadisticas son...");
+        System.out.println("");
+        System.out.print("Vida : "+Stats.get(0));
+        System.out.print(" Fuerza : "+Stats.get(1));
+        System.out.print(" Suerte : "+Stats.get(2));
+        System.out.println("");
+        System.out.print("¿Deseas informacion sobre tus estadisticas? > ");
+        String OP = Read.next();
+        if (OP.equals("Si")||OP.equals("si")||OP.equals("sI")||OP.equals("SI")){
+          Jugabilidad.DSTATS();
+        } else if(OP.equals("No")||OP.equals("no")||OP.equals("nO")||OP.equals("NO")){
+          System.out.println("Muy bien... prosigamos.");
+        } else {
+          System.out.println("");
+        }
+        break;
+      }
+
+    } catch(Exception E){ // Cualquier error implica GAME OVER
+      System.out.println("Aveces... los seres humanos requieren tiempo para que sus decisiones sean correctas...");
+      Jugabilidad.GAMEOVER();
+    }
+    
+    return DatosJ;
+  }
+
+  public static ArrayList Estadisticas(int STR,int VIT,int LUCK){
+    ArrayList Stats = new ArrayList();
+    int Vida = 10+(10/VIT+(VIT));
+    int Fuerza = 1+STR;
+    int Suerte = (LUCK*10)/5;
+    Stats.add(Vida); //Vida
+    Stats.add(Fuerza); //Fuerza
+    Stats.add(Suerte); //Suerte
+    return Stats;
+  }
+
+  public static void DSTATS(){
+    System.out.println("");
+    System.out.println("Muy bien...");
+    // Fuerza
+    System.out.println("Fuerza");
+    System.out.println("La fuerza te ayudara a destruir a tus enemigos.");
+    System.out.println("Realizar ciertas acciones y a su ves sortear obstaculos que requieran fuerza...");
+    System.out.println("");
+    // Vida 
+    System.out.println("Vida");
+    System.out.println("Basicamente esto es tu vida.");
+    System.out.println("Determina la cantidad de golpes que puedes resistir antes de morir.");
+    System.out.println("");
+    // Suerte
+    System.out.println("Suerte");
+    System.out.println("Esta es la estadistica más importante para el juego.");
+    System.out.println("(Almenos en esta ALFA)");
+    System.out.println("Determina la probabilidad de acertar y a su ves,la probabilidad de escapar de una batalla.");
+    System.out.println("Basicamente lo mas crucial.");
+    System.out.println("");
+    System.out.println("Esto es todo lo que debes saber...");
   }
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,61 @@
 import java.util.ArrayList;
-
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    Jugabilidad Juego = new Jugabilidad();
-    Jugabilidad.Batalla();
+    Scanner Read = new Scanner(System.in);
+    System.out.println("¡Bienvenido!");
+    System.out.println("Este 'JUEGO' se encuentra actualmente en estado 'ALFA'");
+    System.out.print("Porfavor... Ingrese su nombre. > ");
+    String Nombre = Read.next();
+    Jugabilidad.Jugador(Nombre);
+
+    
+
+
+    int[] Espacio = new int[4];
+    boolean Control = true;
+    int M = 0;
+    int N = 0;
+    String Respuesta = "";
+    for (int x = 0;x<=3;x++){
+      Espacio[x] = x;
+    }
+    try {
+      while (Control == true){
+      System.out.println("Piso : "+Espacio[M]);
+      System.out.println("¿Avanzas?");
+      Respuesta = Read.nextLine();
+      switch(Respuesta){
+        case "Si":
+        if (M < 3){
+          System.out.println("Avanzas");
+          Jugabilidad.AciertoBatalla();
+          M++;
+        } else if(M == 3) {
+          System.out.println("Ya no hay camino que recorrer.");
+          System.out.println("¿Quieres devolverte?");
+          Respuesta = Read.nextLine();
+          if (Respuesta.equals("Si")){
+            M = 0;
+          } else if(Respuesta.equals("No")) {
+            System.out.println("Como desees...");
+            Control = false;
+          } else {
+            System.out.println("¿Aun no te decides?");
+          }
+        }
+        break;
+        case "No":
+        System.out.println("Como desees...");
+        Control = false;
+        break;
+        default:
+          System.out.println("¿Aun no te decides?");
+        break;
+        }
+      }
+    } catch(Exception e){
+
+    }
   }
 }
